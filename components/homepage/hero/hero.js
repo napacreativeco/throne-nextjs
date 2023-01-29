7541f4523a4190ac9c8a6a5e12af72956043254a
thronestudio <contact@throne.work>
2023-01-29 01:05:55 -0800

    fixes

--- components/homepage/hero/hero.js
+++ components/homepage/hero/hero.js
@@ -86,7 +86,7 @@ const HeroStyles = styled.div`
             gap: 10px;
         }
         .text span.accent {
-            line-height: 1.05;
+            line-height: 1.03;
         }
 
         .row {
